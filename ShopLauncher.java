92b6a5a34ba279f899328c1531c77a62261b558d
Hyeonmin Choi <hyeonminchoi5@gmail.com>
2020-02-18 23:57:03 +0900

    Update ShopLauncher.java

--- ShopLauncher.java
+++ ShopLauncher.java
@@ -3,7 +3,7 @@ package mall;
 public class ShopLauncher {
 
 	public static void main(String[] args) {
-		IShop shop = new MyShop();
+		IShop shop = new MyShop(); //객체 생성후 받아오기
 		shop.setTitle("My Shop");
 		shop.genUser();
 		shop.genProduct();
