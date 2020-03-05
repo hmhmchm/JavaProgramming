ac96723464ac2f8432ea6839b736569f93b10a20
Hyeonmin Choi <hyeonminchoi5@gmail.com>
2020-03-05 22:23:30 +0900

    Create Omok.java

---
+++ Omok.java
@@ -0,0 +1,11 @@
+public class Omok {
+    public static void main(String[] args) {
+        Player user = new Player("사용자", "O");
+        Player computer = new Player("컴퓨터", "X");
+        Board board = new Board(19);
+        play(board, user, computer);
+    }
+    private static void play(Board board, Player user, Player computer) {
+        board.print();
+    }
+}// 보드랑 플레이어 
