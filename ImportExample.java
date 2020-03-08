ef322beeee463009e93ed975a3255d4bc36a53fe
Hyeonmin Choi <hyeonminchoi5@gmail.com>
2020-03-08 22:51:14 +0900

    Create ImportExample.java

---
+++ ImportExample.java
@@ -0,0 +1,15 @@
+import static java.lang.System.out;
+import static java.lang.Math.*;
+ 
+public class ImportExample {
+    public static void main(String[] args) {
+        // System.out.println(Math.random());
+        out.println(random());
+ 
+        // System.out.println(Math.random());
+        out.println("Math.PI : " + PI);
+     }
+}
+
+
+출처: https://deftkang.tistory.com/47 [deftkang의 IT]
